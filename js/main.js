0a3071a71070c98a070fe70167d4c96bcab3b8ac
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-11-04 01:29:25 +0100

    creazione css e js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict"
+// 1. Creare un ciclo for che generi le 100 caselle della griglia
+// 2. Creare un bottone che generi la griglia
+// 3. Generare un array di 16 numeri casuali tra 1 e il numero di caselle
+// 4. Verificare che ogni numero generato non sia già nell'array
+// 5. Ad ogni click su una casella verificare se il numero della casella è nell'array e in tal caso colorarla di rosso
+// 6. Far terminare la partita se un numero dlla lista viene cliccaro o se si clicca su tutti i numeri non presenti
+// 7. Creare un contatore per il punteggio
